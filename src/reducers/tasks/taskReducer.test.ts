55d1d4671b205bc28b8e964994d50da0ba86ddda
DY-JS <delo1604@ukr.net>
2023-07-22 15:32:57 +0200

    Add task tests

--- src/reducers/tasks/taskReducer.test.ts
+++ src/reducers/tasks/taskReducer.test.ts
@@ -1,7 +1,14 @@
 import {v4} from "uuid";
 
 import {TasksStateType} from "../../App";
-import {ADD_TASK, CHANGE_TASK_STATUS, CHANGE_TASK_TITLE, REMOVE_TASK, UnionTasksACType,} from "./tasksActionCreators";
+import {
+    ADD_EMPTY_TASK_ARRAY,
+    ADD_TASK,
+    CHANGE_TASK_STATUS,
+    CHANGE_TASK_TITLE,
+    REMOVE_TASK,
+    UnionTasksACType,
+} from "./tasksActionCreators";
 import {taskReducer} from "./taskReducer";
 
 test('correct task should be removed', () => {
@@ -43,11 +50,13 @@ test('correct task should be added', () => {
     const startState: TasksStateType = {
         [todolistId1]: [
             {id: v4(), title: "HTML&CSS", isDone: true},
-            {id: v4(), title: "JS", isDone: true}
+            {id: v4(), title: "JS", isDone: true},
+            { id: v4(), title: "React", isDone: false }
         ],
         [todolistId2]: [
             {id: v4(), title: "Milk", isDone: true},
-            {id: v4(), title: "React Book", isDone: true}
+            {id: v4(), title: "React Book", isDone: true},
+            { id: v4(), title: "tea", isDone: false }
         ]
     }
 
@@ -61,8 +70,12 @@ test('correct task should be added', () => {
 
         }})
 
-    expect(endState[todolistId1].length).toBe(3)
+    expect(endState[todolistId1].length).toBe(4)
     expect(endState[todolistId1][0].title).toBe(newTaskTitle)
+    expect(endState[todolistId2].length).toBe(3);
+    expect(endState[todolistId2][0].id).toBeDefined();
+    expect(endState[todolistId2][0].title).toBe("Milk");
+    expect(endState[todolistId2][0].isDone).toBe(true);
 })
 
 test('correct task should change its title', () => {
@@ -94,6 +107,7 @@ test('correct task should change its title', () => {
 
     const endState = taskReducer(startState, changeAction)
     expect(endState[todolistId1][0].title).toBe("HTML&CSS")
+    expect(endState[todolistId2][0].id).toBeDefined();
     expect(endState[todolistId1][1].title).toBe(newTaskTitle)
 })
 
@@ -127,29 +141,32 @@ test('correct task should change its status', () => {
     const endState = taskReducer(startState, changeAction)
     expect(endState[todolistId1][0].isDone).toBe(true)
     expect(endState[todolistId1][1].isDone).toBe(false)
+    expect(endState[todolistId2][0].id).toBeDefined();
+    expect(endState[todolistId2][1].isDone).toBe(true)
 })
-//
-// test('correct filter of todolist should be changed', () => {
-//     let todolistId1 = v4()
-//     let todolistId2 = v4()
-//
-//     let newFilter: FilterValuesType = 'completed'
-//
-//     const startState: Array<TodolistType> = [
-//         {id: todolistId1, title: 'What to learn', filter: 'all'},
-//         {id: todolistId2, title: 'What to buy', filter: 'all'}
-//     ]
-//
-//     const action: SetFilterACType = {
-//         type: SET_FILTER,
-//         payload: {
-//             filter: newFilter,
-//             listId: todolistId2
-//         }
-//     }
-//
-//     const endState = todoListReducer(startState, action)
-//
-//     expect(endState[0].filter).toBe('all')
-//     expect(endState[1].filter).toBe(newFilter)
-// })
+
+test('correctly add empty array of tasks  to todolist', () => {
+    let todolistId1 = v4()
+    let todolistId2 = v4()
+
+    const startState: TasksStateType = {
+        [todolistId1]: [
+            {id: v4(), title: "HTML&CSS", isDone: true},
+            {id: v4(), title: "JS", isDone: true}
+        ]
+    }
+
+    const action: UnionTasksACType = {
+        type: ADD_EMPTY_TASK_ARRAY,
+        payload: {
+            listId: todolistId2,
+        }
+    }
+
+    const endState = taskReducer(startState, action)
+    expect(endState[todolistId2]).toStrictEqual([])
+    expect(Object.keys(endState).length).toBe(2)
+})
+
+
+
